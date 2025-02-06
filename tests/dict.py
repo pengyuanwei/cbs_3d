479a75eaed5c9834bbd25c62704a55ccff75f616
Pengyuan Wei <1527755386@qq.com>
2025-02-06 11:25:34 +0000

    Update .gitignore and remove tracked file

--- tests/dict.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-a = dict((i, np.array([1, 1])) for i in range(4))
-
-print(a)
--- tests/test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-static_obstacles = []
-np_static_obstacles = np.array(static_obstacles)
-
-if np_static_obstacles.shape[0] == 0:
-    print("A")
